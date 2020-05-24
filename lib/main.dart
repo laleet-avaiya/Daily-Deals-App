a21496f454a723a8b999dfcce553867c6e07a320
lmavaiya <42064785+lmavaiya@users.noreply.github.com>
2020-05-24 19:13:56 +0530

    admob added

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,16 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:time_ago_provider/time_ago_provider.dart';
+import 'package:firebase_admob/firebase_admob.dart';
 import 'model/Post.dart';
 
 void main() => runApp(MyApp());
+
+// You can also test with your own ad unit IDs by registering your device as a
+// test device. Check the logs for your device's ID value.
+const String AD_MOB_APP_ID = 'ca-app-pub-8894739064593802~2924171610';
+const String AD_MOB_AD_ID = 'ca-app-pub-8894739064593802/9106436587';
+const String AD_MOB_TEST_DEVICE ='9BD99794EFFFC5BD85BE8BB42E0E6525';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -35,6 +42,33 @@ class _MyHomePageState extends State<MyHomePage> {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   String _message = '';
   List<Post> postList = [];
+
+  static const MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+    testDevices: AD_MOB_TEST_DEVICE != null ? <String>[AD_MOB_TEST_DEVICE] : null,
+    childDirected: true,
+    nonPersonalizedAds: true,
+  );
+
+  BannerAd _bannerAd;
+  bool _adShown;
+
+  BannerAd createBannerAd() {
+    return BannerAd(
+      adUnitId: AD_MOB_AD_ID,
+      // adUnitId: BannerAd.testAdUnitId,
+      size: AdSize.banner,
+      targetingInfo: targetingInfo,
+      listener: (MobileAdEvent event) {
+        if (event == MobileAdEvent.loaded) {
+          _adShown = true;
+          setState(() {});
+        } else if (event == MobileAdEvent.failedToLoad) {
+          _adShown = false;
+          setState(() {});
+        }
+      },
+    );
+  }
 
   _register() {
     _firebaseMessaging.getToken().then((token) => print(token));
@@ -90,6 +124,18 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     getMessage();
     loadData();
+    // FirebaseAdMob.instance.initialize(appId: FirebaseAdMob.testAppId);
+     FirebaseAdMob.instance.initialize(appId: AD_MOB_APP_ID);
+    _adShown = false;
+    _bannerAd = createBannerAd()
+      ..load()
+      ..show();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
   }
 
   _launchURL(url) async {
@@ -102,22 +148,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> fakeBottomButtons = new List<Widget>();
+    fakeBottomButtons.add(new Container(
+      height: 50.0,
+    ));
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: new Container(
-          child: postList.length == 0
-              ? Center(
-                  child:
-                      CircularProgressIndicator(backgroundColor: Colors.teal))
-              : new ListView.builder(
-                  itemCount: postList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return PostUI(postList[index]);
-                  },
-                ),
-        ));
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: new Container(
+        child: postList.length == 0
+            ? Center(
+                child: CircularProgressIndicator(backgroundColor: Colors.teal))
+            : new ListView.builder(
+                itemCount: postList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return PostUI(postList[index]);
+                },
+              ),
+      ),
+      persistentFooterButtons: fakeBottomButtons,
+    );
   }
 
   Widget PostUI(Post post) {
@@ -170,7 +222,9 @@ class _MyHomePageState extends State<MyHomePage> {
                       softWrap: true,
                       style: TextStyle(fontSize: 12.0, height: 1.6),
                     ),
-                    Text("",style: TextStyle(fontSize: 12.0, height: 0.2),
+                    Text(
+                      "",
+                      style: TextStyle(fontSize: 12.0, height: 0.2),
                     ),
                     Badge(
                       badgeColor: Colors.teal,
